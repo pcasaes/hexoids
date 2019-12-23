9b6cf6fc4c9935c26d13591032c2e68421365190
Paulo Casaes <paulo.casaes@zwift.com>
2019-12-23 00:44:58 -0300

    Use one thread per kafka topic partition

--- src/main/java/me/paulo/casaes/bbop/service/kafka/KafkaConsumerService.java
+++ src/main/java/me/paulo/casaes/bbop/service/kafka/KafkaConsumerService.java
@@ -17,11 +17,13 @@ import javax.annotation.PreDestroy;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Dependent
@@ -33,11 +35,7 @@ public class KafkaConsumerService {
 
     private final DtoProcessorService dtoProcessorService;
 
-    private Consumer<String, String> kafkaConsumer;
-
-    private java.util.function.Consumer<DomainEvent> processor;
-
-    private boolean running;
+    private List<KafkaThreadedConsumer> threads = Collections.synchronizedList(new ArrayList<>());
 
     @Inject
     public KafkaConsumerService(KafkaConfiguration configuration, DtoProcessorService dtoProcessorService) {
@@ -47,80 +45,131 @@ public class KafkaConsumerService {
 
 
     public void start(String topic, java.util.function.Consumer<DomainEvent> processor) {
-        this.processor = processor;
-
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.configuration.getConnectionUrl());
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
-        this.kafkaConsumer = new KafkaConsumer<>(properties);
-        Collection<TopicPartition> partitions = this.kafkaConsumer.partitionsFor(topic)
-                .stream()
-                .map(pInfo -> new TopicPartition(pInfo.topic(), pInfo.partition()))
-                .collect(Collectors.toList());
-
-        this.kafkaConsumer.assign(partitions);
-        this.kafkaConsumer.seekToBeginning(partitions);
-        new Thread(this::run, "kafka-consumer-" + topic).start();
+        try (Consumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties)) {
+            kafkaConsumer.partitionsFor(topic)
+                    .stream()
+                    .map(pInfo -> new TopicPartition(pInfo.topic(), pInfo.partition()))
+                    .map(topicPartition -> KafkaThreadedConsumer
+                            .startWithoutSubscription(
+                                    properties,
+                                    topicPartition,
+                                    dtoProcessorService,
+                                    processor))
+                    .forEach(threads::add);
+        }
     }
 
     @PreDestroy
     void stop() {
-        this.running = false;
-        kafkaConsumer.wakeup();
+        threads
+                .forEach(KafkaThreadedConsumer::stop);
     }
 
-    private void run() {
-        this.running = true;
-        Duration pollDuration = Duration.ofMillis(1000);
 
-        try {
-            this.pollWhileRunning(pollDuration);
-        } catch (WakeupException ex) {
-            LOGGER.info("Received shutdown signal");
-        } catch (RuntimeException ex) {
-            LOGGER.log(Level.WARNING, ex.getMessage(), ex);
-        } finally {
-            closeConsumer();
+    private static class KafkaThreadedConsumer {
+
+        private final DtoProcessorService dtoProcessorService;
+
+        private final java.util.function.Consumer<DomainEvent> processor;
+
+        private final Consumer<String, String> kafkaConsumer;
+
+        private Thread thread;
+
+        private boolean running;
+
+        private KafkaThreadedConsumer(DtoProcessorService dtoProcessorService,
+                                      java.util.function.Consumer<DomainEvent> processor,
+                                      Consumer<String, String> kafkaConsumer) {
+            this.dtoProcessorService = dtoProcessorService;
+            this.processor = processor;
+            this.kafkaConsumer = kafkaConsumer;
         }
-        LOGGER.info("Consumer stopped");
-    }
 
-    private void pollWhileRunning(Duration pollDuration) {
-        while (this.running) {
-            ConsumerRecords<String, String> records = kafkaConsumer.poll(pollDuration);
-            if (!records.isEmpty()) {
-                StreamSupport
-                        .stream(
-                                records.spliterator(),
-                                false)
-                        .forEach(this::process);
+        static KafkaThreadedConsumer startWithoutSubscription(Properties properties,
+                                                              TopicPartition topicPartition,
+                                                              DtoProcessorService dtoProcessorService,
+                                                              java.util.function.Consumer<DomainEvent> processor) {
+
+            Consumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+
+
+            Collection<TopicPartition> partitions = Collections.singletonList(topicPartition);
+            kafkaConsumer.assign(partitions);
+            kafkaConsumer.seekToBeginning(partitions);
+
+            KafkaThreadedConsumer kafkaThreadedConsumer = new KafkaThreadedConsumer(
+                    dtoProcessorService,
+                    processor,
+                    kafkaConsumer);
+
+            kafkaThreadedConsumer.thread = new Thread(kafkaThreadedConsumer::run, "kafka-consumer-" + topicPartition.topic());
+            kafkaThreadedConsumer.thread.start();
+
+            return kafkaThreadedConsumer;
+        }
+
+        void stop() {
+            this.running = false;
+            kafkaConsumer.wakeup();
+        }
+
+        private void run() {
+            this.running = true;
+            Duration pollDuration = Duration.ofMillis(1000);
+
+            try {
+                this.pollWhileRunning(pollDuration);
+            } catch (WakeupException ex) {
+                LOGGER.info("Received shutdown signal");
+            } catch (RuntimeException ex) {
+                LOGGER.log(Level.WARNING, ex.getMessage(), ex);
+            } finally {
+                closeConsumer();
+            }
+            LOGGER.info("Consumer stopped");
+        }
+
+        private void pollWhileRunning(Duration pollDuration) {
+            while (this.running) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(pollDuration);
+                if (!records.isEmpty()) {
+                    StreamSupport
+                            .stream(
+                                    records.spliterator(),
+                                    false)
+                            .forEach(this::process);
+                }
             }
         }
-    }
 
-    private void closeConsumer() {
-        try {
-            kafkaConsumer.close(Duration.ofSeconds(5));
-        } catch (KafkaException ex) {
-            LOGGER.log(Level.WARNING, "Could not close connection", ex);
-        }
-    }
-
-    private void process(ConsumerRecord<String, String> record) {
-        try {
-            if (record.value() == null) {
-                this.processor.accept(DomainEvent.deleted(record.key()));
-            } else {
-                dtoProcessorService.getEventType(record.value())
-                        .ifPresent(eventType -> {
-                            EventDto event = dtoProcessorService.deserialize(record.value(), eventType.getClassType());
-                            this.processor.accept(DomainEvent.of(record.key(), event));
-                        });
+        private void closeConsumer() {
+            try {
+                kafkaConsumer.close(Duration.ofSeconds(5));
+            } catch (KafkaException ex) {
+                LOGGER.log(Level.WARNING, "Could not close connection", ex);
             }
-        } catch (RuntimeException ex) {
-            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+
+        private void process(ConsumerRecord<String, String> record) {
+            try {
+                if (record.value() == null) {
+                    this.processor.accept(DomainEvent.deleted(record.key()));
+                } else {
+                    dtoProcessorService.getEventType(record.value())
+                            .ifPresent(eventType -> {
+                                EventDto event = dtoProcessorService.deserialize(record.value(), eventType.getClassType());
+                                this.processor.accept(DomainEvent.of(record.key(), event));
+                            });
+                }
+            } catch (RuntimeException ex) {
+                LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+            }
         }
     }
 
