19b226a1b4d93b8d548eecf30961f3f26281844a
Paulo Casaes <paulo.casaes@zwift.com>
2019-12-30 17:16:00 -0300

    Refactor move queue producer in game client
    Former-commit-id: ad1ded17de2158a7a813ccc8a17fbcd74178ac6c

--- src/main/resources/META-INF/resources/js/event-queue.js
+++ src/main/resources/META-INF/resources/js/event-queue.js
@@ -1,4 +1,4 @@
-class EventQueue {
+class QueueConsumer {
     constructor(typeProperty) {
         this.typeProperty = typeProperty;
         this.events = {};
@@ -14,5 +14,46 @@ class EventQueue {
         if (event) {
             event(resp);
         }
+    }
+}
+
+
+class QueueProducer {
+    constructor(sendMessage) {
+        this.queue = [];
+        this.sendMessage = sendMessage;
+        this.intervalId = null;
+    }
+
+    produce(message) {
+        this.queue.push(message);
+    }
+
+    start() {
+        this.intervalId  = setInterval(() => {
+            const m = {
+                "command": "MOVE_PLAYER",
+                "moveX": 0,
+                "moveY": 0,
+            };
+
+            let hasAngle = false;
+            for (let evt = this.queue.shift(); !!evt; evt = this.queue.shift()) {
+                if (evt.move) {
+                    m.moveX += evt.move.x;
+                    m.moveY += evt.move.y;
+                }
+                if (evt.angle) {
+                    m.angle = evt.angle.value;
+                    hasAngle = true;
+                }
+                m.thrustAngle = evt.thrustAngle;
+            }
+            if (hasAngle || m.moveX !== 0 || m.moveY !== 0) {
+                this.sendMessage(m);
+            }
+        }, 50);
+
+        return this;
     }
 }
