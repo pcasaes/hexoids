9ba093da059c073da414c9757f61c6178b52f6c5
Paulo Casaes <paulo.casaes@zwift.com>
2019-12-30 02:02:48 -0300

    Use object pool for bolts
    Former-commit-id: 8e25b1627178afb519de3cd66be5a8b025af4a4c

--- src/main/resources/META-INF/resources/js/bolts.js
+++ src/main/resources/META-INF/resources/js/bolts.js
@@ -3,6 +3,8 @@ const Bolts = (function () {
 
     const PULSE = [0xfffff, 0x888888, 0x888888, 0x000000];
 
+    const POOL = [];
+
 
     class Bolt {
         constructor(data) {
@@ -11,13 +13,17 @@ const Bolts = (function () {
             this.bg = null;
             this.pulsePos = 0;
             this.owner = null;
+
+            this.isNew = true;
         }
 
         create(b) {
             const move = this.data.transform.view(b.x, b.y);
 
-            this.sprite = this.data.scene.physics.add.image(move.x, move.y, 'bolt');
-            this.bg = this.data.scene.physics.add.image(move.x, move.y, 'bolt');
+            if (this.isNew) {
+                this.sprite = this.data.scene.physics.add.image(move.x, move.y, 'bolt');
+                this.bg = this.data.scene.physics.add.image(move.x, move.y, 'bolt');
+            }
 
             this.owner = this.data.players[b.ownerPlayerId];
             if (this.owner) {
@@ -25,16 +31,27 @@ const Bolts = (function () {
                 this.bg.setTint(this.owner.ship.color, this.owner.ship.color, this.owner.ship.color, this.owner.ship.color);
                 this.owner.ship.fireBolt.generate();
             }
-            this.sprite.setBounce(0, 0);
-            this.sprite.setScale(0.15);
-            this.sprite.setDepth(this.data.gameConfig.add('ship.depth', 1));
-            this.sprite.setCollideWorldBounds(true);
 
-            this.bg.setBounce(0, 0);
-            this.bg.setScale(1);
-            this.bg.setCollideWorldBounds(true);
-            this.bg.setDepth(this.data.gameConfig.background.effectsDepth);
-            this.bg.setAlpha(0.7);
+            if (this.isNew) {
+                this.sprite.setBounce(0, 0);
+                this.sprite.setScale(0.15);
+                this.sprite.setDepth(this.data.gameConfig.add('ship.depth', 1));
+                this.sprite.setCollideWorldBounds(true);
+
+                this.bg.setBounce(0, 0);
+                this.bg.setScale(1);
+                this.bg.setCollideWorldBounds(true);
+                this.bg.setDepth(this.data.gameConfig.background.effectsDepth);
+                this.bg.setAlpha(0.7);
+                this.isNew = false;
+            } else {
+                this.sprite
+                    .setActive(true)
+                    .setVisible(true);
+                this.bg
+                    .setActive(true)
+                    .setVisible(true);
+            }
 
             return this;
         }
@@ -60,8 +77,14 @@ const Bolts = (function () {
         }
 
         destroy() {
-            this.sprite.destroy();
-            this.bg.destroy();
+            this.sprite
+                .setActive(false)
+                .setVisible(false);
+            this.bg
+                .setActive(false)
+                .setVisible(false);
+
+            POOL.push(this);
         }
     }
 
@@ -79,7 +102,9 @@ const Bolts = (function () {
 
         move(b) {
             if (!this.bolts[b.boltId]) {
-                this.bolts[b.boltId] = new Bolt(this.data).create(b);
+                const bolt = POOL.pop();
+
+                this.bolts[b.boltId] = (!bolt ? new Bolt(this.data): bolt).create(b);
             } else {
                 this.bolts[b.boltId].move(b);
             }
