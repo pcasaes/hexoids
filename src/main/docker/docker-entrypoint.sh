c626f5621d741d3c23aba9559e321de58664d10d
Paulo Casaes <pcasaes@gmail.com>
2020-01-31 22:34:06 -0300

    Configure prefer native transport for vertx

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 echo "starting"
+
 # If no parameter, run the game server
 if [ $# -eq 0 ]; then
   if [ -z $JAVA_EXTRA_OPTS ]; then
@@ -8,6 +9,8 @@ if [ $# -eq 0 ]; then
   else
     JAVA_OPTIONS="${JAVA_OPTIONS} ${JAVA_EXTRA_OPTS}"
   fi
+  IP_ADDRESS=`hostname -i`
+  JAVA_OPTIONS="${JAVA_OPTIONS} -Dquarkus.http.domain-socket-enabled=true -Dquarkus.http.domain-socket=/deployments/sockets/${IP_ADDRESS}.socket"
   echo "JAVA_OPTIONS: ${JAVA_OPTIONS}"
   exec /deployments/run-java.sh
 fi
