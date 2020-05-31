3de85333dc80d0e47cf9e7f7c793dc53a720a023
Paulo Casaes <pcasaes@gmail.com>
2020-05-31 11:37:19 -0300

    Move topic creation to docker backed script

--- src/main/java/me/pcasaes/hexoids/service/kafka/KafkaInitService.java
+++ src/main/java/me/pcasaes/hexoids/service/kafka/KafkaInitService.java
@@ -2,10 +2,6 @@ package me.pcasaes.hexoids.service.kafka;
 
 import io.quarkus.runtime.StartupEvent;
 import me.pcasaes.hexoids.model.GameTopic;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.common.KafkaFuture;
-import org.apache.kafka.common.errors.TopicExistsException;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -17,14 +13,9 @@ import javax.inject.Inject;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.function.BooleanSupplier;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @ApplicationScoped
 public class KafkaInitService {
@@ -39,8 +30,6 @@ public class KafkaInitService {
     private Instance<KafkaConsumerService> kafkaConsumerServiceFactory;
     private Map<GameTopic, KafkaConsumerService> kafkaConsumerServiceMap = new ConcurrentHashMap<>();
 
-    private KafkaAdmin kafkaAdmin;
-
     KafkaInitService() {
         //required for CDI normal scoped beans
     }
@@ -48,11 +37,9 @@ public class KafkaInitService {
     @Inject
     public KafkaInitService(KafkaService kafkaService,
                             @Any Instance<TopicInfo> topicsFactory,
-                            KafkaAdmin kafkaAdmin,
                             Instance<KafkaConsumerService> kafkaConsumerServiceFactory) {
         this.kafkaService = kafkaService;
         this.topicsFactory = topicsFactory;
-        this.kafkaAdmin = kafkaAdmin;
         this.kafkaConsumerServiceFactory = kafkaConsumerServiceFactory;
     }
 
@@ -63,11 +50,6 @@ public class KafkaInitService {
     @PostConstruct
     void start() {
         LOGGER.info("Starting up kafka consumers");
-        try {
-            this.kafkaAdmin.execute(this::setup);
-        } catch (RuntimeException ex) {
-            LOGGER.log(Level.WARNING, "Error while initializing topics", ex);
-        }
 
         for (TopicInfoPriority.Priority priority : TopicInfoPriority.Priority.values()) {
             List<BooleanSupplier> starting = this.topicsFactory
@@ -109,58 +91,6 @@ public class KafkaInitService {
         kafkaConsumerServiceMap
                 .values()
                 .forEach(kafkaConsumerServiceFactory::destroy);
-    }
-
-
-    public void init() {
-        LOGGER.info("Initializing topics");
-    }
-
-    private void setup(AdminClient adminClient) {
-        List<NewTopic> newTopics = StreamSupport
-                .stream(topicsFactory.spliterator(),
-                        false)
-                .map(this::getNewTopics)
-                .collect(Collectors.toList());
-
-        adminClient
-                .createTopics(newTopics)
-                .values()
-                .entrySet()
-                .forEach(this::handelNewTopicCreation);
-
-    }
-
-    private NewTopic getNewTopics(TopicInfo topic) {
-        try {
-            return topic.newTopic();
-        } finally {
-            this.topicsFactory.destroy(topic);
-        }
-    }
-
-    private void handelNewTopicCreation(Map.Entry<String, KafkaFuture<Void>> entry) {
-        final KafkaFuture<Void> future = entry.getValue();
-        final String topic = entry.getKey();
-
-        try {
-            future.get(10, TimeUnit.SECONDS);
-            if (LOGGER.isLoggable(Level.INFO)) {
-                LOGGER.info("Topic " + topic + " created");
-            }
-
-        } catch (InterruptedException ex) {
-            LOGGER.warning(ex.getMessage());
-            Thread.currentThread().interrupt();
-        } catch (TimeoutException ex) {
-            LOGGER.log(Level.SEVERE, "Could not create topic " + topic, ex);
-        } catch (ExecutionException ex) {
-            if (ex.getCause() instanceof TopicExistsException) {
-                LOGGER.fine("Topic " + topic + " already exists: " + ex.getMessage());
-            } else {
-                LOGGER.log(Level.SEVERE, "Could not create topic " + topic, ex);
-            }
-        }
     }
 
 }
--- src/main/java/me/pcasaes/hexoids/service/kafka/topics/ScoreBoardUpdateTopic.java
+++ src/main/java/me/pcasaes/hexoids/service/kafka/topics/ScoreBoardUpdateTopic.java
@@ -3,33 +3,15 @@ package me.pcasaes.hexoids.service.kafka.topics;
 import me.pcasaes.hexoids.model.GameTopic;
 import me.pcasaes.hexoids.service.kafka.TopicInfo;
 import me.pcasaes.hexoids.service.kafka.TopicInfoPriority;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.common.config.TopicConfig;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Named;
-import java.util.HashMap;
-import java.util.Map;
 
 @Dependent
 @Named("ScoreBoardUpdateTopic")
 @TopicInfoPriority(TopicInfoPriority.Priority.TWO)
 public class ScoreBoardUpdateTopic implements TopicInfo {
 
-    @Override
-    public NewTopic newTopic() {
-        final Map<String, String> props = new HashMap<>();
-        props.put(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(60_000));
-        props.put(TopicConfig.DELETE_RETENTION_MS_CONFIG, String.valueOf(1_000));
-        props.put(TopicConfig.SEGMENT_MS_CONFIG, String.valueOf(60_000));
-        props.put(TopicConfig.SEGMENT_BYTES_CONFIG, String.valueOf(1024 * 1024 * 128));
-        props.put(TopicConfig.MIN_CLEANABLE_DIRTY_RATIO_CONFIG, "0.25");
-        props.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT + ", " + TopicConfig.CLEANUP_POLICY_DELETE);
-        props.put(TopicConfig.MESSAGE_TIMESTAMP_TYPE_CONFIG, "CreateTime");
-
-        return new NewTopic(topic().name(), 1, (short) 1)
-                .configs(props);
-    }
 
     @Override
     public GameTopic topic() {
