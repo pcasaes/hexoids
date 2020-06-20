2d2bd70b4eb811f9621e66c9ca8b6760a4457167
Paulo Casaes <pcasaes@gmail.com>
2020-06-20 19:44:00 -0300

    Remove entrypoint metrics

--- src/main/java/me/pcasaes/hexoids/entrypoints/web/CommandDelegate.java
+++ src/main/java/me/pcasaes/hexoids/entrypoints/web/CommandDelegate.java
@@ -2,7 +2,6 @@ package me.pcasaes.hexoids.entrypoints.web;
 
 import me.pcasaes.hexoids.core.application.commands.ApplicationCommands;
 import me.pcasaes.hexoids.core.domain.model.EntityId;
-import org.eclipse.microprofile.metrics.annotation.Counted;
 import pcasaes.hexoids.proto.JoinCommandDto;
 import pcasaes.hexoids.proto.MoveCommandDto;
 
@@ -19,38 +18,18 @@ class CommandDelegate {
         this.applicationCommands = applicationCommands;
     }
 
-    @Counted(
-            name = "bolt-fire-command",
-            tags = "layer=entry-point",
-            absolute = true
-    )
     void fire(EntityId userId) {
         this.applicationCommands.getFireCommand().fire(userId);
     }
 
-    @Counted(
-            name = "leave-command",
-            tags = "layer=entry-point",
-            absolute = true
-    )
     void leave(EntityId userId) {
         this.applicationCommands.getLeaveGameCommand().leave(userId);
     }
 
-    @Counted(
-            name = "spawn-command",
-            tags = "layer=entry-point",
-            absolute = true
-    )
     void spawn(EntityId userId) {
         this.applicationCommands.getSpawn().spawn(userId);
     }
 
-    @Counted(
-            name = "join-command",
-            tags = "layer=entry-point",
-            absolute = true
-    )
     void join(EntityId userId, JoinCommandDto command) {
         this.applicationCommands.getJoinGameCommand().join(userId, command);
     }
