8766206bfefd506ad4c38d7f73adba57075c1317
Paulo Casaes <pcasaes@gmail.com>
2020-06-21 05:45:28 -0300

    Use Instant for HRClock

--- src/main/java/me/pcasaes/hexoids/infrastructure/clock/HRClock.java
+++ src/main/java/me/pcasaes/hexoids/infrastructure/clock/HRClock.java
@@ -1,11 +1,14 @@
 package me.pcasaes.hexoids.infrastructure.clock;
 
+import java.time.Instant;
+
 public final class HRClock {
 
-    private static final Long START_TIME_SECONDS = System.currentTimeMillis() / 1000L;
+    private static final long START_TIME_SECONDS = System.currentTimeMillis() / 1000L;
 
     public static long nanoTime() {
-        return System.nanoTime();
+        Instant now = Instant.now();
+        return (now.getEpochSecond() - START_TIME_SECONDS) * 1_000_000_000L + now.getNano();
     }
 
     private HRClock() {
