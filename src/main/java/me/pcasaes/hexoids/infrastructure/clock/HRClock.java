843c88cc3ee62409511011dac995ab94b832877a
Paulo Casaes <pcasaes@gmail.com>
2020-06-21 05:52:50 -0300

    Revert to using System.nanoTime

--- src/main/java/me/pcasaes/hexoids/infrastructure/clock/HRClock.java
+++ src/main/java/me/pcasaes/hexoids/infrastructure/clock/HRClock.java
@@ -1,14 +1,9 @@
 package me.pcasaes.hexoids.infrastructure.clock;
 
-import java.time.Instant;
-
 public final class HRClock {
 
-    private static final long START_TIME_SECONDS = System.currentTimeMillis() / 1000L;
-
     public static long nanoTime() {
-        Instant now = Instant.now();
-        return (now.getEpochSecond() - START_TIME_SECONDS) * 1_000_000_000L + now.getNano();
+        return System.nanoTime();
     }
 
     private HRClock() {
