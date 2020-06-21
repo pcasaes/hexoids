36c40f5c4a985f3953ea0c7aee61b9d020cce7e0
Paulo Casaes <pcasaes@gmail.com>
2020-06-21 04:56:28 -0300

    Revert to System.nanoTime()

--- src/main/java/me/pcasaes/hexoids/infrastructure/clock/HRClock.java
+++ src/main/java/me/pcasaes/hexoids/infrastructure/clock/HRClock.java
@@ -1,29 +1,11 @@
 package me.pcasaes.hexoids.infrastructure.clock;
 
-import java.lang.reflect.Method;
-
 public final class HRClock {
 
     private static final Long START_TIME_SECONDS = System.currentTimeMillis() / 1000L;
 
-    private static final Method TIME_METHOD;
-
-    static {
-        try {
-            Class cls = Class.forName("jdk.internal.misc.VM");
-            TIME_METHOD = cls.getMethod("getNanoTimeAdjustment", Long.TYPE);
-            TIME_METHOD.setAccessible(true);
-        } catch (ReflectiveOperationException ex) {
-            throw new IllegalStateException(ex);
-        }
-    }
-
     public static long nanoTime() {
-        try {
-            return ((Number) TIME_METHOD.invoke(null, START_TIME_SECONDS)).longValue();
-        } catch (ReflectiveOperationException ex) {
-            throw new IllegalStateException(ex);
-        }
+        return System.nanoTime();
     }
 
     private HRClock() {
