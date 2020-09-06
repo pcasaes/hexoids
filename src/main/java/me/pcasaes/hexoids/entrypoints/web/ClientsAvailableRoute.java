4991f00b2affc077615c7e330d6d6be299904d9b
Paulo Casaes <pcasaes@gmail.com>
2020-09-06 02:33:43 -0300

    Remove client caching from clients available endpoint

---
+++ src/main/java/me/pcasaes/hexoids/entrypoints/web/ClientsAvailableRoute.java
@@ -0,0 +1,43 @@
+package me.pcasaes.hexoids.entrypoints.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RouteBase;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.HashMap;
+import java.util.Map;
+
+@ApplicationScoped
+@RouteBase(path = "clients", produces = "application/json")
+public class ClientsAvailableRoute {
+
+    private static final String AVAILABLE;
+
+    static {
+        Map<String, String> versions = new HashMap<>();
+
+        versions.put("OSX", "0.1.0");
+        versions.put("Windows", "0.1.0");
+        versions.put("X11", "0.1.0");
+        versions.put("HTML5", "0.1.0");
+
+        try {
+            AVAILABLE = new ObjectMapper().writeValueAsString(versions);
+        } catch (JsonProcessingException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    @Route(path = "available", methods = HttpMethod.GET)
+    public void available(RoutingContext rc) {
+        rc.response()
+                .putHeader("Cache-Control", "no-cache, no-store, must-revalidate")
+                .putHeader("Pragma", "no-cache")
+                .putHeader("Expires", "0")
+                .end(AVAILABLE);
+    }
+}
