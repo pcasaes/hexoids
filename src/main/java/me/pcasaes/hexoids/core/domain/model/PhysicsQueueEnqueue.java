641ce5d22777cb588a51fccea7afc26fb3b36b51
Paulo Casaes <pcasaes@gmail.com>
2022-03-31 00:26:47 -0300

    Document PhysicsQueueEnqueue

--- src/main/java/me/pcasaes/hexoids/core/domain/model/PhysicsQueueEnqueue.java
+++ src/main/java/me/pcasaes/hexoids/core/domain/model/PhysicsQueueEnqueue.java
@@ -4,6 +4,10 @@ import java.util.function.LongPredicate;
 
 public interface PhysicsQueueEnqueue {
 
+    /**
+     * Enqueues an action into the physics queue.
+     * @param action    Receives timestamp. On return true will re-enqueue to execute on next physics iteration
+     */
     void enqueue(LongPredicate action);
 
 }
