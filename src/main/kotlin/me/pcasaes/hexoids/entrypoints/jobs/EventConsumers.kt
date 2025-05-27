a0d8b5900c55c79c7eeee56f12b535e472d06e64
Paulo Casaes <pcasaes@gmail.com>
2025-05-26 19:41:24 -0700

    Cleanup code

--- src/main/kotlin/me/pcasaes/hexoids/entrypoints/jobs/EventConsumers.kt
+++ src/main/kotlin/me/pcasaes/hexoids/entrypoints/jobs/EventConsumers.kt
@@ -1,7 +1,6 @@
 package me.pcasaes.hexoids.entrypoints.jobs
 
 import io.quarkus.runtime.StartupEvent
-import io.smallrye.mutiny.tuples.Tuple3
 import jakarta.annotation.Priority
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
@@ -10,11 +9,7 @@ import jakarta.interceptor.Interceptor
 import me.pcasaes.hexoids.core.application.eventhandlers.ApplicationConsumers
 import me.pcasaes.hexoids.core.domain.model.DomainEvent
 import me.pcasaes.hexoids.core.domain.model.EventRecord
-import org.eclipse.microprofile.reactive.messaging.Acknowledgment
 import org.eclipse.microprofile.reactive.messaging.Incoming
-import org.eclipse.microprofile.reactive.messaging.Message
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.CompletionStage
 import java.util.logging.Logger
 
 @ApplicationScoped
@@ -25,49 +20,33 @@ class EventConsumers @Inject constructor(private val applicationConsumers: Appli
 
 
     @Incoming("join-game")
-    @Acknowledgment(Acknowledgment.Strategy.NONE)
-    fun onJoinGame(message: Message<EventRecord>): CompletionStage<Unit> {
-        applicationConsumers.onJoinGame(toDomainEvent(message.getPayload()))
-        return CompletableFuture.completedFuture(Unit)
+    fun onJoinGame(eventRecord: EventRecord) {
+        applicationConsumers.onJoinGame(toDomainEvent(eventRecord))
     }
 
     @Incoming("player-action")
-    @Acknowledgment(Acknowledgment.Strategy.NONE)
-    fun onPlayerAction(message: Message<EventRecord>): CompletionStage<Unit> {
-        applicationConsumers.onPlayerAction(toDomainEvent(message.getPayload()))
-        return CompletableFuture.completedFuture(Unit)
+    fun onPlayerAction(eventRecord: EventRecord) {
+        applicationConsumers.onPlayerAction(toDomainEvent(eventRecord))
     }
 
     @Incoming("bolt-life-cycle")
-    @Acknowledgment(Acknowledgment.Strategy.NONE)
-    fun onBoltLifeCycle(message: Message<EventRecord>): CompletionStage<Unit> {
-        applicationConsumers.onBoltLifeCycle(toDomainEvent(message.getPayload()))
-        return CompletableFuture.completedFuture(Unit)
+    fun onBoltLifeCycle(eventRecord: EventRecord) {
+        applicationConsumers.onBoltLifeCycle(toDomainEvent(eventRecord))
     }
 
     @Incoming("bolt-action")
-    @Acknowledgment(Acknowledgment.Strategy.NONE)
-    fun onBoltAction(message: Message<EventRecord>): CompletionStage<Unit> {
-        applicationConsumers.onBoltAction(toDomainEvent(message.getPayload()))
-        return CompletableFuture.completedFuture(Unit)
+    fun onBoltAction(eventRecord: EventRecord) {
+        applicationConsumers.onBoltAction(toDomainEvent(eventRecord))
     }
 
     @Incoming("score-board-control")
-    @Acknowledgment(Acknowledgment.Strategy.MANUAL)
-    fun onScoreBoardControl(message: Message<EventRecord>): CompletionStage<Unit> {
-        try {
-            applicationConsumers.onScoreBoardControl(toDomainEvent(message.getPayload()))
-        } finally {
-            message.ack()
-        }
-        return CompletableFuture.completedFuture(Unit)
+    fun onScoreBoardControl(eventRecord: EventRecord) {
+        applicationConsumers.onScoreBoardControl(toDomainEvent(eventRecord))
     }
 
     @Incoming("score-board-update")
-    @Acknowledgment(Acknowledgment.Strategy.NONE)
-    fun onScoreBoardUpdate(message: Message<EventRecord>): CompletionStage<Unit> {
-        applicationConsumers.onScoreBoardUpdate(toDomainEvent(message.getPayload()))
-        return CompletableFuture.completedFuture(Unit)
+    fun onScoreBoardUpdate(eventRecord: EventRecord) {
+        applicationConsumers.onScoreBoardUpdate(toDomainEvent(eventRecord))
     }
 
     private fun toDomainEvent(consumerRecord: EventRecord): DomainEvent {
@@ -80,7 +59,5 @@ class EventConsumers @Inject constructor(private val applicationConsumers: Appli
 
     companion object {
         private val LOGGER: Logger = Logger.getLogger(EventConsumers::class.java.getName())
-
-        private val NONE: Tuple3<String?, Int?, Long?> = Tuple3.of<String?, Int?, Long?>("", 0, 0L)
     }
 }
